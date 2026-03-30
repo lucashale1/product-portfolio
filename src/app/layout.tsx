f443b48383d33ab7097e1ec57af1386305d69983
lucashale1 <lucas.hale10@gmail.com>
2026-03-30 09:13:46 +0100

    Added Vercel Speed Insights.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@once-ui-system/core/css/styles.css";
 import "@once-ui-system/core/css/tokens.css";
 import "@/resources/custom.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import classNames from "classnames";
 
@@ -112,6 +113,7 @@ export default async function RootLayout({
           </Flex>
           <Footer />
           <Analytics />
+          <SpeedInsights />
         </Column>
       </Providers>
     </Flex>
